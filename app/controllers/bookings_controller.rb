c32a8ef88dc0a807fc9f2093d96b9ed9b67e1c4e
aditya <aditya.nicboy@gmail.com>
2023-05-04 16:56:34 +0530

    Download spreadsheet

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,31 @@ class BookingsController < ApplicationController
   def my_bookings
     @bookings = current_user.bookings
   end
+
+  def bookings_index
+    @bookings = current_user.bookings
+
+    respond_to do |format|
+      format.html
+      format.xlsx do
+        response.headers['Content-Disposition'] = 'attachment; filename="bookings.xlsx"'
+        response.headers['Content-Type'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        send_data generate_xlsx_data(@bookings), filename: 'bookings.xlsx', disposition: 'attachment'
+      end
+    end
+  end
+
+  private
+
+  def generate_xlsx_data(bookings)
+    Axlsx::Package.new do |p|
+      p.workbook.add_worksheet(name: 'Bookings') do |sheet|
+        sheet.add_row ['Book Title', 'Author', 'Booking Date']
+
+        bookings.each do |booking|
+          sheet.add_row [booking.book.title, booking.book.author, booking.created_at]
+        end
+      end
+    end.to_stream.read
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   post 'books/:id/book', to: 'books#create_booking', as: 'create_booking_book'
   get 'my_bookings', to: 'bookings#my_bookings'
+  get 'bookings', to: 'bookings#bookings_index', as: 'bookings_index'
 
   resources :books do
     post :book, on: :member
